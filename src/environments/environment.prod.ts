ba74bdf00284118f7d75f577339b5c09ac9adf77
aventris <ipo778@hotmail.com>
2022-03-02 08:46:26 -0600

    UPDATE. Production enviroment variables

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,9 @@
 export const environment = {
-  production: true
+  production: true,
+  USER_NAME: 'jimmie_k',
+  USER_PASSWORD: 'klein*#%*',
+  USER_ID: 10,
+  ADMIN_USER_NAME: 'hopkins',
+  ADMIN_USER_PASSWORD: 'William56$hj',
+  ADMIN_USER_ID: 8,
 };
